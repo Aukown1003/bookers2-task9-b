f690e9277c60474f8212cc9fc4b838f32453f78a
YsukeKuwano <115121908+Aukown1003@users.noreply.github.com>
2022-11-23 04:01:28 +0000

    [Start]bookers2-task7-a

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,9 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  # favoritesの持つuserの情報を抜き出す！
+  has_many :favorited_users, through: :favorites, source: :user
+  
   has_many :book_comments, dependent: :destroy
   
   # favoriteで使用するメソッドの定義
